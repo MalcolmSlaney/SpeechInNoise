5b6eee771d6cc965d359f5b1d7dcb9ab6d48ca80
Malcolm Slaney <malcolm@ieee.org>
2026-05-04 13:55:37 -0700

    Adding OOV penalty to offline_asr

--- run_exp1c.sh
+++ run_exp1c.sh
@@ -9,11 +9,10 @@ dir=exp1/exp1_${prompt}_results
 python migration.py --dbfile $dbfile
 python clear_single_word_asr.py --dbfile $dbfile --nodry_run
 python offline_asr.py --dbfile $dbfile --single_word_projects="$project_list" \
-  --num_workers 6 --use_forced --valid_words valid_words.json --oov_penalty 0.0  --debug > $dir/offline_asr.log
+  --num_workers 6 --use_forced --valid_words valid_words.json --oov_penalty 10.0  --debug > $dir/offline_asr.log
 
 # Now that we have the ASR results, run the analysis and save the results.
 for prompt in forced; do
   mkdir -p $dir
   python analyze_results.py --dbfile experiments_exp1_${prompt}.db --debug_count=100000 > $dir/analysis.txt
   mv asr_audiology_discrepancies.html confusion_matrices.png quicksin_results.csv $dir/
-done
