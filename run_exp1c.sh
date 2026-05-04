a418320c5aa79fa01d711cf7438e7a89d31140bd
Malcolm Slaney <malcolm@ieee.org>
2026-05-03 19:11:15 -0700

    Preserving offline_asr results

--- run_exp1c.sh
+++ run_exp1c.sh
@@ -4,14 +4,15 @@ rm -f $dbfile
 cp experiments.db $dbfile
 chmod 644 $dbfile
 
+dir=exp1/exp1_${prompt}_results
+
 python migration.py --dbfile $dbfile
 python clear_single_word_asr.py --dbfile $dbfile --nodry_run
 python offline_asr.py --dbfile $dbfile --single_word_projects="$project_list" \
-  --num_workers 6 --use_forced --valid_words valid_words.json
+  --num_workers 6 --use_forced --valid_words valid_words.json --oov_penalty 0.0  --debug > $dir/offline_asr.log
 
 # Now that we have the ASR results, run the analysis and save the results.
 for prompt in forced; do
-  dir=exp1/exp1_${prompt}_results
   mkdir -p $dir
   python analyze_results.py --dbfile experiments_exp1_${prompt}.db --debug_count=100000 > $dir/analysis.txt
   mv asr_audiology_discrepancies.html confusion_matrices.png quicksin_results.csv $dir/
